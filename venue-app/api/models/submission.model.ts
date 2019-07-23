7b9c7ad1b3eb31159cd612505b0c279fe04bae48
ndeahl1999 <deahln@rpi.edu>
2019-07-23 14:38:29 -0400

    new models

---
+++ venue-app/api/models/submission.model.ts
@@ -0,0 +1,45 @@
+var mongoose = require('bluebird').promisifyAll(require('mongoose'));
+var Schema = mongoose.Schema;
+import Section from './section.model';
+import SectionEvent from './sectionevent.model';
+var GeoJSON = require('mongoose-geojson-schema');
+
+var SubmissionSchema = new Schema({
+  content: String,
+  images: [String], // path to image on static image server?
+  time: Date,
+  submitter: {type : Schema.Types.ObjectId, ref: 'User'},
+  authors: [{type : Schema.Types.ObjectId, ref: 'User'}],
+  sectionEvent: {type : Schema.Types.ObjectId, ref: 'SectionEvent'},
+  instructorApproval: {
+    instructor: {type : Schema.Types.ObjectId, ref: 'User'},
+    time: Date,
+    comments: String
+  },
+  instructorVerification: { type: String, enum: ["verified", "rejected", "none"] },
+  verified: Boolean,
+  locationMatch: Boolean,
+  location: {
+    geo: mongoose.Schema.Types.Point
+  }
+},{ timestamps: true});
+
+SubmissionSchema.index({ 'location.geo' : '2dsphere'});
+
+SubmissionSchema.pre("save",function(next) {
+  next();
+});
+
+SubmissionSchema.methods = {
+
+  getSectionAsync: function(){
+    return SectionEvent.findByIdAsync(this.sectionEvent)
+      .then((sectionEvent) => {
+        return Section.findByIdAsync(sectionEvent.section);
+      });
+  }
+
+};
+
+export default mongoose.model('Submission', SubmissionSchema);
+module.exports = mongoose.model('Submission', SubmissionSchema);
