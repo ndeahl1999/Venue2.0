e6f8c48957282294eab1e55ba803ddb3f0a3be27
tdurkin18 <durkim@rpi.edu>
2019-07-26 13:53:13 -0400

    re-did signup page

--- venue-app/src/app/signup/signup.component.ts
+++ venue-app/src/app/signup/signup.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { UsersService } from '../users.service';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,27 +11,45 @@ import { UsersService } from '../users.service';
 })
 export class SignupComponent implements OnInit {
 
-  angForm: FormGroup
-  constructor(private fb: FormBuilder, private us: UsersService) {
-      this.createForm();
-  }
+  constructor(
+      private formBuilder: FormBuilder,
+      private router: Router,
+      private toastr: ToastrService,
+      private userService: UserService
+    ) { }
+    registerForm: FormGroup;
+    loading = false;
+    submitted = false;
 
-  createForm(){
-    this.angForm = this.fb.group({
-     firstName: ['', Validators.required ],
-     lastName: ['', Validators.required ],
-     email: ['', Validators.required ],
-     password: ['', Validators.required ],
+    ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required,Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    }
 
-   });
-  }
+    get fval() { return this.registerForm.controls; }
 
-
-  addUser(firstName, lastName, email, password){
-    console.log("adding");
-    this.us.addUser(firstName, lastName, email, password);
-  }
-  ngOnInit() {
-  }
-
+    onFormSubmit(){
+      this.submitted = true;
+      // return for here if form is invalid
+      if (this.registerForm.invalid) {
+        console.log("invalid form");
+        return;
+      }
+      console.log("submitting form");
+      this.loading = true;
+      this.userService.register(this.registerForm.value).subscribe(
+        (data) => {
+          alert('Account has been created!');
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          this.toastr.error(error.error.message, 'Error');
+          this.loading = false;
+        }
+      )
+    }
 }
